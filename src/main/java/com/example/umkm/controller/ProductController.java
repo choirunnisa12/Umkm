6db8e5e47d738184a6a8acee814fa2d45d02f2ed
CHOIRUNNISA <choirunnisa1821@gmail.com>
2024-07-18 19:36:28 +0700

    update product controller

--- src/main/java/com/example/umkm/controller/ProductController.java
+++ src/main/java/com/example/umkm/controller/ProductController.java
@@ -1,4 +1,50 @@
 package com.example.umkm.controller;
 
+import com.example.umkm.entity.Product;
+import com.example.umkm.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
 public class ProductController {
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Product request) {
+        Product create = productService.create(request);
+        if (create != null) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(create);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to create product");
+        }
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Integer id) {
+        return productService.getById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Product update(@RequestBody Product request) {
+        return productService.update(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        productService.delete(id);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
 }
